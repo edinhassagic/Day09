df4e30e307b64053378db5e77f19c20182f8780e
edin hasagic <edinhassagic@gmail.com>
2021-03-17 02:49:20 +0100

    idontknow

---
+++ ex15/src/index.js
@@ -0,0 +1,14 @@
+function myDoWhile() {
+var i = 0;
+do {
+    console.log(i);
+    i++;
+}
+while (i < 7) {
+    console.log(i);
+    i++;
+}
+
+}
+console.log(myDoWhile);
+module.exports = myDoWhile;
